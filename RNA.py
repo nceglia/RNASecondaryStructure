98c7989e4686d06d243ecb324533f015471ffba0
chang27 <chanl27@uci.edu>
2016-11-29 17:19:35 -0800

    Update RNA.py

--- RNA.py
+++ RNA.py
@@ -12,8 +12,17 @@ def StructureDomain(sequence):
     2 = Interior Loop
     3 = K-Multiloop
     """
-    return [xrange(4) for _ in xrange(size)]
+    #return [xrange(4) for _ in xrange(size)]
+    return [range(10) for _ in range(100)]
 
+def TestCost(graph):
+    cost = 0
+    prev = graph[0]
+    for i in graph[1:]:
+        if (prev == i):
+            cost = cost + 1
+        prev = i
+    return cost
 
 def GibbsFreeEnergy(configuration):
     totalEnergy = 0
