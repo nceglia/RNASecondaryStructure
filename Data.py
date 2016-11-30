d554820da60a1cb54f2cc7307c1387b694675bc2
chang27 <chanl27@uci.edu>
2016-11-29 17:17:57 -0800

    Update Data.py

--- Data.py
+++ Data.py
@@ -7,10 +7,14 @@ seq1="ACUUCGCAAGCACGCGUAGGGAAAGGCACCAUGUAUCACGAUAUUACAUACUAAGAGCGU\
 CAACGUGAAUACCUGCUGGAUACUGUGUGGGCCGUGGUGAAAGUUUGAUCCGCAAAGCAG\
 CCCCUGUAACUGUACUCGCGGCAAGAGCAUCGCAGCAGUAUGUGCGUCUGAAUGCGACAC\
 GGAAGGCACGGCGGGACCCA"
-
+testSq = [random.randint(0, 10) for i in range(100)]
 def Data(object):
     def __init__(self,source):
         self.source = source
 
     def getSequence(self):
-        return seq1
+        #return seq1
+        return testSq
+    
+
+    
